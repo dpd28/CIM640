bd720a51e0907d50e3762771154637da9e1a9fd2
Deb Pang Davis <dxp785@miami.edu>
2018-09-19 23:50:37 -0400

    added bow tie, background, text interactions

--- Homework/p5/drawing/sketch.js
+++ Homework/p5/drawing/sketch.js
@@ -3,11 +3,20 @@ These are global variables. placement at the top.
 First, declare variables. Then, add the value.
 */
 
+
+// var greeting = true;
+var greeting = "Hi, I’m Oliver.";
+var goodbye = "I’m sleepy. Good night."
+var question = "¿Cómo se llama?";
+
 // ColorStyles
 var pupilColor = 'black';
 var feetColor = 'black';
 var faceColor = 'white';
 var tieColor = 'rgb(163,0,0)';
+var r = 67;
+var g = 142;
+var b = 172;
 
 // Position it
 var facePosY = 313;
@@ -15,6 +24,10 @@ var toePosX = 475;
 var toePosY = 695;
 
 // Size Styles
+var bodyX = 562.5;
+var bodyY = 450;
+var bodyWidth = 450;
+var bodyHeight = 516;
 var eyeWidth = 164;
 var eyeHeight = 125;
 var pupilWidth = 34;
@@ -22,7 +35,6 @@ var pupilHeight = 34;
 var toeWidth = 25;
 var midToeHeight = 55;
 var outToeHeight = 45;
-
 /* ------
 End Variables
 */
@@ -30,20 +42,51 @@ End Variables
 function setup() {
   // put setup code here
     createCanvas(windowWidth, windowHeight);
-    background(67,142,172);
-}
+} // end setup
+
+
 
 function draw() {
+  background(r,g,b); // connected to variable
+
   // put drawing code here
     strokeWeight(0)
   // beginShape (); and make sure endShape
+  fill(255);
+
+  textSize(32);
+  textFont('Georgia');
+//   text("Hello, I’m Oliver.", 850,150);
+//
+// if(mouseIsPressed){
+//   text(question, 850, 200);
+// }
 
   // body
     fill (217,208,193);
-    ellipse (562.5, 450, 426, 516);
+    ellipse (bodyX, bodyY, bodyWidth, bodyHeight);
+    var mouseOverOwl = false;
+    var bodyDist = dist(bodyX, bodyY, mouseX, mouseY);
+    console.log ("bodyDist: " + bodyDist);
+
+if(bodyDist < 225){
+  mouseOverOwl = true;
+      r = 0;
+      g = 51;
+      b = 102;
+      pupilColor=(112,128,144);
+
+}  else {
+  mouseOverOwl = false;
+    r=67;
+    g=142;
+    b=172;
+    pupilColor=('black');
+}
+
   // ears
-    triangle (320,175,550,210,390,340);
-    triangle (600,210,800,175,720,340);
+    triangle (420,155,550,210,390,340);
+    triangle (600,210,700,155,720,340);
 
   // face
     fill(faceColor);
@@ -64,6 +107,13 @@ function draw() {
     ellipse(563,520,20,20);
     triangle(563,520,605,500,605,540);
 
+    if(mouseX >=520 && mouseX <=605){
+      tieColor=('black');
+    } else {
+      tieColor=('rgb(163,0,0)');
+    }
+
+
   // first set of feet
     fill(0);
     ellipse (toePosX, toePosY, toeWidth, outToeHeight); // outToe one
@@ -74,4 +124,16 @@ function draw() {
     ellipse (toePosX+150, toePosY, toeWidth, midToeHeight); // Midtoe two
     ellipse (toePosX+175, toePosY, toeWidth, outToeHeight); // outToe three
 
+    if(mouseOverOwl == false){
+      fill(255);
+      text(greeting, 850,150);
+    } else {
+      fill('rgb(255,215,0)')
+      text(goodbye, 850, 200);
+    }
+
 } // do not delete!
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
