99f15f95bec5469470e66075985b1eccbb5d7b12
Deb Pang Davis <dxp785@miami.edu>
2018-12-03 12:45:42 -0500

    Madness I say! Extra function in the splits.

--- Homework/p5/final/sketch.js
+++ Homework/p5/final/sketch.js
@@ -6,6 +6,8 @@ var bgcolor;
 var poemButton;
 
 var rg; // testing with Dan Schiffman riGrammar
+var rgr; // replace with these words
+var rg7;
 
 // This is an array of words to replace 1 syllables
 var replaceOne = ["joy", "owls", "dog", "light", "limbs", "fate", "snow", "blood"];
@@ -21,6 +23,8 @@ var replaceFour = ["tears in your eyes", ];
 
 // This is an array of words to replace 5 syllables
 var replaceFive = ["moonlight sonata", "soprano shivers", "frazzled daisy spins"];
+
+var recheck = false;
 
 function preload() {
     myFont = loadFont('assets/fonts/PERPETUASTD.OTF')
@@ -50,9 +54,33 @@ rg.addRule('<2>', 'people | yellow | citrus | content | evening | faces | beguil
 rg.addRule('<3>','piano | tequila | nocturnal | abundant | fiddleheads | caresses | idyllic | peony | evenings | ladybug | surfaces');
 rg.addRule('<4>','eternity | alien | alligator | thunderstorm brews |  gold caresses | woo you away');
 rg.addRule('<5>', 'applause fills the air | bubbles laugh out loud | spring hopes melt away | broken wings quiver | breezes surf blood leaves | courage rests in peace');
-var result = rg.expand();
-console.log(result);
+//var result = rg.expand();
+
+//console.log(result);
 lines = ["click to", "generate", "a haiku"];
+
+
+//////////////
+// Replace with these rules
+//////////////
+rgr = new RiGrammar();
+rgr.addRule('<start>', '<5-line>');
+rgr.addRule('<5-line>', '<1> <4> | <1> <3> <1> | <2> <3>');
+rgr.addRule('<1>','joy | owls | dog | light | limbs | fate | snow | stars');
+rgr.addRule('<2>', 'music | water | healing | feather | drowning | goosebumps');
+rgr.addRule('<3>','discover | enlighten | peony | gardenia | rough waters | light kisses');
+rgr.addRule('<4>','tears in your eyes | celebration | vegetable | beautiful tone | avocado');
+rgr.addRule('<5>', 'moonlight sonata | soprano shivers | frazzled daisy spins');
+
+rg7 = new RiGrammar();
+rg7.addRule('<start>', '<7-line>');
+rg7.addRule('<5-line>', '<1> <4> | <1> <3> <1> | <2> <3>');
+rg7.addRule('<7-line>', '<1> <1> <5-line> | <2> <5-line> | <5-line> <1> <1> | <5-line> <2>');
+rg7.addRule('<1>','joy | owls | dog | light | limbs | fate | snow | stars');
+rg7.addRule('<2>', 'music | water | healing | feather | drowning | goosebumps');
+rg7.addRule('<3>','discover | enlighten | peony | gardenia | rough waters | light kisses');
+rg7.addRule('<4>','tears in your eyes | celebration | vegetable | beautiful tone | avocado');
+rg7.addRule('<5>', 'moonlight sonata | soprano shivers | frazzled daisy spins');
 
 
   textFont(myFont);
@@ -67,7 +95,6 @@ lines = ["click to", "generate", "a haiku"];
   select("#wrapper").style("height", "windowHeight");
   select("#wrapper").style("margin", "0 auto");
   select("#wrapper").style("padding", "20px");
-  select("#wrapper").style("font-family", "Helvetica");
   select("#wrapper").style("text-align", "center");
   var cnv = createCanvas(windowWidth, windowHeight); //this creates a canvas object
     cnv.id("mycanvas");
@@ -99,7 +126,9 @@ function draw() {
   text(lines[1], width / 2, 275);
   text(lines[2], width / 2, 350);
 
-////////////////////////// PATTERN TEST /////////////////////////////
+//////////////////////////
+// PATTERN TEST
+/////////////////////////////
 // noFill();
 noStroke();
 for(var x = 50; x <= width-50; x += 50){
@@ -114,12 +143,43 @@ for(var x = 50; x <= width-50; x += 50){
 function mousePressed () {
   bgcolor = color(random(250, 170), random(250, 170), random(250, 170));
   var result = rg.expand();
-  // var checkHaiku = new RiString(result);
+  // pass into function
+   var checkHaiku = new RiString(result);
+   var haiku = result.split("%");
+   //var test = haiku[0].split(" ");
+   //console.log(test);
+
+   haikuCheck(haiku);
+
   // var replacedHaiku = checkHaiku;
-  // checkHaiku = checkHaiku.words();
+  checkHaiku = checkHaiku.words();
   //
   // console.log(checkHaiku);
-  // for(var words in checkHaiku){
+   //   for (var i = 0; i < haiku.length; i++){
+   //     for(var words in checkHaiku){
+   //       if(haiku[i].search(checkHaiku[words]) > -1){
+   //         //find a new line
+   //         //haiku[i] needs to change
+   //         //haiku[i] = rgr.expand();
+   //         //if haiku i == 0 or 2 5 line rule
+   //         //if haiku i == 1 7 rule
+   //         //recheck
+   //
+   //         if(i == 0 || i == 2){
+   //           haiku[i] = rgr.expand();
+   //           recheck = true;
+   //           console.log("changed");
+   //         }else if(i == 1){
+   //           haiku[i] = rg7.expand();
+   //           recheck = true;
+   //           console.log("changed");
+   //
+   //         }
+   //         break;
+   //       }
+   //
+   //   }
+   // }
   //   var currentWord = checkHaiku[words];
   //   // currentword = fruit
   //   for(var loopAgain in checkHaiku){
@@ -133,12 +193,33 @@ function mousePressed () {
   //
   //   }
   // }
-  var haiku = result.split("%");
-  for (var i = 0; i < lines.length; i++)
+  for (var i = 0; i < lines.length; i++){
     lines[i] = haiku[i];
+  }
 } // End mousePressed
 
 
 // function windowResized() {
 //   resizeCanvas(windowWidth, windowHeight);
 // }
+
+function haikuCheck(currentHaiku){
+  var wordsArray = [];
+
+  // wordsArray.push(currentHaiku[1].split(" "));
+  // wordsArray.push(currentHaiku[2].split(" "));
+  for(var lines in currentHaiku){
+    if(lines < 3){
+      var currentLine = currentHaiku[lines].split(" ");
+      //console.log(currentLine);
+       for(var words in currentLine){
+        if(currentLine[words] != ""){
+          wordsArray.push(currentLine[words]);
+        }
+      }
+    }
+
+  }
+
+  console.log(wordsArray);
+}
